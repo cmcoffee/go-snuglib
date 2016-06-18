829b64da446b70305d0e4c2d33a5ed206b07f07c
Craig M. Coffee <excelsior@docker>
2016-06-18 00:29:17 -0700

    Applied gofmt to cfg.go.

--- cfg.go
+++ cfg.go
@@ -23,9 +23,9 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
-	"strconv"
 )
 
 type Store struct {
@@ -53,7 +53,9 @@ func (s *Store) Get(section, key string) []string {
 	if result, found := s.cfgStore[section][key]; !found {
 		return []string{EMPTY}
 	} else {
-		if len(result) == 0 { return []string{EMPTY} }
+		if len(result) == 0 {
+			return []string{EMPTY}
+		}
 		return result
 	}
 }
@@ -88,10 +90,14 @@ func (s *Store) Exists(input ...string) (found bool) {
 	defer s.mutex.RUnlock()
 
 	inlen := len(input)
-	if inlen == 0 { return false}
+	if inlen == 0 {
+		return false
+	}
 
 	if inlen > 0 {
-		if _, found = s.cfgStore[input[0]]; found { return }
+		if _, found = s.cfgStore[input[0]]; found {
+			return
+		}
 	}
 	if inlen > 1 {
 		if found == true {
@@ -110,11 +116,15 @@ func (s *Store) Set(section, key string, value ...string) (err error) {
 	key = strings.ToLower(key)
 	var newValue []string
 
-	for _, val := range value { newValue = append(newValue, val) }
+	for _, val := range value {
+		newValue = append(newValue, val)
+	}
 
 	// If read-only, do not write to file.
 	if !s.readOnly {
-		if err := SetFile(s.file, section, key, newValue[0:]...); err != nil { return err }
+		if err := SetFile(s.file, section, key, newValue[0:]...); err != nil {
+			return err
+		}
 	}
 
 	// Create new map if one doesn't exist.
@@ -144,7 +154,9 @@ func cfgErr(file string, line int) error {
 // Creates a new empty config file & Store, overwriting an existing file with comments if specified.
 func Create(file string, comment ...string) (out *Store, err error) {
 	f, err := os.Create(file)
-	if err != nil { return nil, err }
+	if err != nil {
+		return nil, err
+	}
 
 	defer f.Close()
 
@@ -165,7 +177,9 @@ func Create(file string, comment ...string) (out *Store, err error) {
 // Reads configuration file and returns Store, changes are not saved to disk.
 func ReadOnly(file string) (out *Store, err error) {
 	out, err = Load(file)
-	if out != nil { out.readOnly = true }
+	if out != nil {
+		out.readOnly = true
+	}
 	return
 }
 
@@ -196,23 +210,31 @@ scanLoop:
 		line++
 		txt := s.Text() + "\n"
 		l := len(txt)
-		if l < 2 { continue }
+		if l < 2 {
+			continue
+		}
 
 		for i, ch := range txt {
 			skip = false
 			switch ch {
 			case '\n':
-				if flag&cfg_KEY == 0 { return out, cfgErr(file, line) }
+				if flag&cfg_KEY == 0 {
+					return out, cfgErr(file, line)
+				}
 				if flag&cfg_COMMA != 0 {
 					flag &^= cfg_COMMA
 					continue
 				}
-				if flag&cfg_ESCAPE > 0 { buf.WriteRune('\\') }
+				if flag&cfg_ESCAPE > 0 {
+					buf.WriteRune('\\')
+				}
 				flag &^= cfg_HEADER | cfg_KEY | cfg_ESCAPE
 				addVal(buf, &val)
 				for i, v := range val {
 					unq, err := strconv.Unquote(fmt.Sprintf("\"%s\"", v))
-					if err == nil { val[i] = unq }
+					if err == nil {
+						val[i] = unq
+					}
 				}
 				out.cfgStore[section][key] = val
 				val = nil
@@ -223,7 +245,9 @@ scanLoop:
 					flag |= cfg_ESCAPE
 					continue
 				}
-				if !skip { skip = true }
+				if !skip {
+					skip = true
+				}
 				fallthrough
 			case ',':
 				if flag&cfg_KEY > 0 && flag&cfg_ESCAPE == 0 {
@@ -232,8 +256,10 @@ scanLoop:
 					flag |= cfg_COMMA
 					continue
 				}
-				flag&^=cfg_ESCAPE 
-				if !skip { skip = true }
+				flag &^= cfg_ESCAPE
+				if !skip {
+					skip = true
+				}
 				fallthrough
 			case '[':
 				if flag&cfg_KEY == 0 && !skip {
@@ -248,11 +274,17 @@ scanLoop:
 						return out, cfgErr(file, line)
 					}
 				}
-				if !skip { skip = true }
+				if !skip {
+					skip = true
+				}
 				fallthrough
 			case '#':
-				if flag&cfg_KEY == 0 && !skip { continue scanLoop }
-				if !skip { skip = true }
+				if flag&cfg_KEY == 0 && !skip {
+					continue scanLoop
+				}
+				if !skip {
+					skip = true
+				}
 				fallthrough
 			case '=':
 				if flag&cfg_KEY == 0 && !skip {
@@ -261,7 +293,9 @@ scanLoop:
 					last = line
 					continue
 				}
-				if !skip { skip = true }
+				if !skip {
+					skip = true
+				}
 				fallthrough
 			default:
 				if buf.Len() == 0 {
@@ -273,16 +307,20 @@ scanLoop:
 					}
 				}
 				flag &^= cfg_COMMA
-				if i == l-1 && buf.Len() != 0 { return out, cfgErr(file, line) }
-				if flag&cfg_ESCAPE > 0 { 
+				if i == l-1 && buf.Len() != 0 {
+					return out, cfgErr(file, line)
+				}
+				if flag&cfg_ESCAPE > 0 {
 					buf.WriteRune('\\')
-					flag&^= cfg_ESCAPE 
+					flag &^= cfg_ESCAPE
 				}
 				buf.WriteRune(ch)
 			}
 		}
 	}
-	if flag&cfg_KEY != 0 { return out, cfgErr(file, last) }
+	if flag&cfg_KEY != 0 {
+		return out, cfgErr(file, last)
+	}
 	return out, nil
 }
 
@@ -299,7 +337,9 @@ func ListSections(file string) (out []string, err error) {
 		txt := s.Text()
 		l := len(txt)
 
-		if l > 1 && txt[0] == '#' || l == 1 { continue }
+		if l > 1 && txt[0] == '#' || l == 1 {
+			continue
+		}
 
 		if l > 2 && txt[0] == '[' && txt[l-1] == ']' {
 			out = append(out[0:], txt[1:l-1])
@@ -312,7 +352,9 @@ func ListSections(file string) (out []string, err error) {
 func ReadFile(file, section string) (out map[string][]string, err error) {
 	section = strings.ToLower(section)
 	f, err := os.Open(file)
-	if err != nil { return nil, err }
+	if err != nil {
+		return nil, err
+	}
 
 	defer f.Close()
 	s := bufio.NewScanner(f)
@@ -330,18 +372,26 @@ func ReadFile(file, section string) (out map[string][]string, err error) {
 		txt := s.Text() + "\n"
 		l := len(txt)
 
-		if l > 1 && txt[0] == '#' || l == 1 { continue }
+		if l > 1 && txt[0] == '#' || l == 1 {
+			continue
+		}
 
 		if flag&cfg_HEADER == 0 {
 
 			// Skip to section sections only.
 			if l > 1 {
-				if !strings.ContainsAny(txt, "[ & ]") { continue }
-			} else { continue }
+				if !strings.ContainsAny(txt, "[ & ]") {
+					continue
+				}
+			} else {
+				continue
+			}
 
 			txt = strings.ToLower(txt)
 
-			if strings.HasPrefix(txt, "["+section+"]") { flag |= cfg_HEADER }
+			if strings.HasPrefix(txt, "["+section+"]") {
+				flag |= cfg_HEADER
+			}
 
 		} else {
 			for i, ch := range txt {
@@ -353,12 +403,16 @@ func ReadFile(file, section string) (out map[string][]string, err error) {
 							flag &^= cfg_COMMA
 							continue
 						}
-						if flag&cfg_ESCAPE > 0 { buf.WriteRune('\\') } 
+						if flag&cfg_ESCAPE > 0 {
+							buf.WriteRune('\\')
+						}
 						flag &^= cfg_KEY | cfg_ESCAPE
 						addVal(buf, &val)
 						for i, v := range val {
 							unq, err := strconv.Unquote(fmt.Sprintf("\"%s\"", v))
-							if err == nil { val[i] = unq }
+							if err == nil {
+								val[i] = unq
+							}
 						}
 						out[key] = val
 						val = nil
@@ -369,7 +423,9 @@ func ReadFile(file, section string) (out map[string][]string, err error) {
 						flag |= cfg_ESCAPE
 						continue
 					}
-					if !skip { skip = true }
+					if !skip {
+						skip = true
+					}
 					fallthrough
 				case '=':
 					if flag&cfg_KEY == 0 && !skip {
@@ -378,7 +434,9 @@ func ReadFile(file, section string) (out map[string][]string, err error) {
 						last = line
 						continue
 					}
-					if !skip { skip = true }
+					if !skip {
+						skip = true
+					}
 					fallthrough
 				case ',':
 					if flag&cfg_KEY > 0 && !skip && flag&cfg_ESCAPE == 0 {
@@ -387,14 +445,20 @@ func ReadFile(file, section string) (out map[string][]string, err error) {
 						flag |= cfg_COMMA
 						continue
 					}
-					if flag&cfg_ESCAPE > 0 { flag &^= cfg_ESCAPE }
-					if !skip { skip = true }
+					if flag&cfg_ESCAPE > 0 {
+						flag &^= cfg_ESCAPE
+					}
+					if !skip {
+						skip = true
+					}
 					fallthrough
 				case '[':
 					if flag&cfg_KEY == 0 && !skip {
 						return
 					}
-					if !skip { skip = true }
+					if !skip {
+						skip = true
+					}
 					fallthrough
 				default:
 					if buf.Len() == 0 {
@@ -406,10 +470,12 @@ func ReadFile(file, section string) (out map[string][]string, err error) {
 						}
 					}
 					flag &^= cfg_COMMA
-					if i == l-1 && buf.Len() != 0 { return out, cfgErr(file, line) }
-					if flag&cfg_ESCAPE > 0 { 
+					if i == l-1 && buf.Len() != 0 {
+						return out, cfgErr(file, line)
+					}
+					if flag&cfg_ESCAPE > 0 {
 						buf.WriteRune('\\')
-						flag&^= cfg_ESCAPE 
+						flag &^= cfg_ESCAPE
 					}
 					buf.WriteRune(ch)
 				}
@@ -417,7 +483,9 @@ func ReadFile(file, section string) (out map[string][]string, err error) {
 			}
 		}
 	}
-	if flag&cfg_KEY != 0 { return out, cfgErr(file, last) }
+	if flag&cfg_KEY != 0 {
+		return out, cfgErr(file, last)
+	}
 	return out, nil
 }
 
@@ -426,12 +494,16 @@ func SetFile(file, section, key string, value ...string) error {
 	section = strings.ToLower(section)
 	key = strings.ToLower(key)
 	f, err := os.Open(file)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer f.Close()
 
 	// Generate temp file, then close it, reopen it with append.
 	tmp, err := ioutil.TempFile(os.TempDir(), fmt.Sprintf("%s.temp_conf.", os.Args[0]))
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	tmpfname := tmp.Name()
 
 	no_end_comma := func(input string) (no_comma bool) {
@@ -504,7 +576,9 @@ func SetFile(file, section, key string, value ...string) error {
 	// Copys line start to line end of src file to dst file.
 	copyFile := func(src, dst *os.File, start, end int) error {
 		_, err := src.Seek(0, 0)
-		if err != nil { return err }
+		if err != nil {
+			return err
+		}
 
 		s := bufio.NewScanner(src)
 		var line int
@@ -517,25 +591,33 @@ func SetFile(file, section, key string, value ...string) error {
 		for (line < end || end == -1) && s.Scan() {
 			line++
 			_, err := dst.WriteString(s.Text() + "\n")
-			if err != nil { return err }
+			if err != nil {
+				return err
+			}
 		}
 		return nil
 	}
 
 	var txt []string
 
-	if flag&cfg_HEADER == 0 { txt = append(txt, "["+section+"]") }
+	if flag&cfg_HEADER == 0 {
+		txt = append(txt, "["+section+"]")
+	}
 
 	var spacer []byte
 
 	for i, str := range value {
-		if str == EMPTY { break }
+		if str == EMPTY {
+			break
+		}
 
 		if i == 0 {
 			str = strings.Replace(strconv.Quote(str), ",", "\\,", -1)
 			txt = append(txt, key+" = "+str[1:len(str)-1])
 			spacer = make([]byte, len(key+" = "))
-			for ch := range spacer { spacer[ch] = ' ' }
+			for ch := range spacer {
+				spacer[ch] = ' '
+			}
 			continue
 		}
 		txt = append(txt, string(spacer)+str)
@@ -543,7 +625,9 @@ func SetFile(file, section, key string, value ...string) error {
 
 	// Appends first half of the file.
 	err = copyFile(f, tmp, 0, head)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	// Inject new section when needed, and key = values.
 	txtL := len(txt) - 1
@@ -551,54 +635,80 @@ func SetFile(file, section, key string, value ...string) error {
 		if i == 0 {
 			if flag&cfg_HEADER == 0 {
 				_, err = tmp.WriteString("\n" + out + "\n")
-				if err != nil { return err }
+				if err != nil {
+					return err
+				}
 				continue
 			}
 		}
 		if i < txtL {
 			_, err = tmp.WriteString(out + ",\n")
-			if err != nil { return err }
+			if err != nil {
+				return err
+			}
 		} else {
 			_, err = tmp.WriteString(out + "\n")
-			if err != nil { return err }
+			if err != nil {
+				return err
+			}
 		}
 	}
 
 	// Appends second half of file.
 	if tail != -1 {
 		err = copyFile(f, tmp, tail-1, -1)
-		if err != nil { return err }
+		if err != nil {
+			return err
+		}
 	}
 
 	// Sync and close everything.
 	err = tmp.Sync()
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	err = tmp.Close()
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	tmp, err = os.Open(tmpfname)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	err = f.Close()
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	destfile, err := os.OpenFile(file, os.O_RDWR|os.O_TRUNC, 0600)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	defer destfile.Close()
 
 	_, err = io.Copy(destfile, tmp)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	err = destfile.Sync()
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	err = tmp.Close()
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	err = os.Remove(tmpfname)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
